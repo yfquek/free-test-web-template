57a4e0b89fba4c3d3f69dd32a42b707de3812993
yfquek <yfquek94@gmail.com>
2022-07-09 12:45:47 +0800

    IGNORE
    IGNORE

--- main.tf
+++ main.tf
@@ -49,7 +49,6 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description = "TLS from VPC"
     description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
